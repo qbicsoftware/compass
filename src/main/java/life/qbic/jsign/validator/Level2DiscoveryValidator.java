000141975e9b2d6839495d6259c5afe22879f4a2
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2025-12-12 09:15:32 +0100

    Add class description for level 2 discovery validator

--- src/main/java/life/qbic/jsign/validator/Level2DiscoveryValidator.java
+++ src/main/java/life/qbic/jsign/validator/Level2DiscoveryValidator.java
@@ -8,9 +8,56 @@ import life.qbic.linksmith.spi.WebLinkValidator.Issue;
 import life.qbic.linksmith.spi.WebLinkValidator.IssueReport;
 
 /**
- * <class short description>
+ * Signposting validator for <strong>Level&nbsp;2 discovery</strong>.
+ * <p>
+ * Level&nbsp;1 Signposting exposes typed links for a scholarly object
+ * <em>inline</em>, for example in HTTP {@code Link} headers or HTML
+ * {@code <link>} elements (e.g. {@code rel="cite-as"}, {@code rel="describedby"},
+ * {@code rel="item"}).
+ * <br>
+ * Level&nbsp;2 Signposting, in contrast, exposes (the same and additional) typed
+ * links <em>by reference</em> through a separate <em>Linkset</em> resource, which
+ * is advertised from the object using {@code rel="linkset"} and a dedicated
+ * media type such as {@code application/linkset} or {@code application/linkset+json}.
+ * </p>
  *
- * @since <version tag>
+ * <p>
+ * This validator focuses <strong>only on discovering</strong> Level&nbsp;2
+ * Linkset resources in an already parsed set of {@link WebLink}s. It:
+ * </p>
+ * <ul>
+ *   <li>looks for links with {@code rel="linkset"},</li>
+ *   <li>checks whether a supported Linkset media type
+ *       ({@code application/linkset} or {@code application/linkset+json}) is present,</li>
+ *   <li>records warnings for missing or unsupported {@code type} parameters, and</li>
+ *   <li>records a warning if multiple supported Linkset targets are advertised.</li>
+ * </ul>
+ *
+ * <p>
+ * The validator <strong>does not</strong> dereference Linkset URIs, perform any
+ * network I/O, or validate the contents of a Linkset document. It only tells you
+ * whether a suitable Linkset entry is discoverable from the given WebLinks.
+ * </p>
+ *
+ * <p>
+ * Downstream, clients are expected to:
+ * </p>
+ * <ol>
+ *   <li>Inspect the {@link SignPostingResult} / {@link IssueReport} to ensure a
+ *       supported Linkset was discovered,</li>
+ *   <li>select one of the advertised Linkset targets and fetch it over HTTP, and</li>
+ *   <li>pass the fetched Linkset content (e.g. {@code application/linkset+json})
+ *       to a dedicated Linkset parser and Level&nbsp;2 profile validator that operate
+ *       on the Linkset’s WebLinks.</li>
+ * </ol>
+ *
+ * <p>
+ * The {@link SignPostingView} returned in the {@link SignPostingResult} simply
+ * wraps the input WebLinks and does not modify or filter them; all discovery
+ * information is conveyed via the recorded {@link Issue}s.
+ * </p>
+ *
+ * @author Sven Fillinger
  */
 public class Level2DiscoveryValidator implements SignPostingValidator {
 
@@ -85,22 +132,55 @@ public class Level2DiscoveryValidator implements SignPostingValidator {
     }
   }
 
+  /**
+   * Verifies if a given web link contains a relation of type {@code linkset}.
+   *
+   * @param webLink the web link to verify
+   * @return true, if the weblink has a relation of type {@code linkset}, else returns false
+   */
   private static boolean hasLinkSetRelation(WebLink webLink) {
     return webLink.rel().stream().anyMatch(Level2DiscoveryValidator::hasLinkSetRelation);
   }
 
+  /**
+   * Verifies if a given serialised relation type is `linkset`.
+   *
+   * @param relation the potential serialised linkset type
+   * @return true, if the linkset type is 'linkset', else returns false
+   */
   private static boolean hasLinkSetRelation(String relation) {
     return relation.equals("linkset");
   }
 
+  /**
+   * Verifies if a given web link has the type parameter.
+   *
+   * @param webLink the weblink to verify
+   * @return true, if the web link has the type parameter, else returns false
+   */
   private static boolean hasType(WebLink webLink) {
     return webLink.type().isPresent();
   }
 
+  /**
+   * Verifies if a given web link has any supported MIME type for a linkset.
+   * <p>
+   * Note: this method will also consider the case of absent type parameter, for which it will
+   * always return false.
+   *
+   * @param webLink the web link to verify for any supported MIME type for linksets
+   * @return true, if the web link contains any supported linkset MIME type, else returns false
+   */
   private static boolean hasSupportedLinkSetType(WebLink webLink) {
     return hasSupportedLinkSetType(webLink.type().orElse(""));
   }
 
+  /**
+   * Verifies if a given serialised type is a supported MIME type for linksets.
+   *
+   * @param type the serialised MIME type to verify
+   * @return true, if the type is a known MIME type for linksets
+   */
   private static boolean hasSupportedLinkSetType(String type) {
     return type.equalsIgnoreCase("application/linkset")
         || type.equalsIgnoreCase("application/linkset+json");
