227751faf9e8b83935cdecc730366b4914a9e736
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2026-01-09 16:32:17 +0100

    Rename level 2 entry validator

--- src/main/java/life/qbic/compass/validation/Level2RecipeValidator.java
+++ src/main/java/life/qbic/compass/validation/Level2RecipeValidator.java
@@ -1,6 +1,7 @@
 package life.qbic.compass.validation;
 
 import java.util.List;
+import java.util.Objects;
 import life.qbic.compass.spi.SignPostingResult;
 import life.qbic.compass.spi.SignPostingValidator;
 import life.qbic.linksmith.model.WebLink;
@@ -10,7 +11,41 @@ import life.qbic.linksmith.model.WebLink;
  *
  * @since <version tag>
  */
-public class Level2RecipeRoutingValidator implements SignPostingValidator {
+public class Level2RecipeValidator implements SignPostingValidator {
+
+  private final SignPostingValidator landingPageValidator;
+  private final SignPostingValidator metadataResourceValidator;
+  private final SignPostingValidator contentResourceValidator;
+
+  private Level2RecipeValidator(
+      SignPostingValidator landingPageValidator,
+      SignPostingValidator metadataResourceValidator,
+      SignPostingValidator contentResourceValidator
+  ) {
+    this.landingPageValidator = Objects.requireNonNull(landingPageValidator);
+    this.metadataResourceValidator = Objects.requireNonNull(metadataResourceValidator);
+    this.contentResourceValidator = Objects.requireNonNull(contentResourceValidator);
+  }
+
+  public static Level2RecipeValidator create() {
+    return new Level2RecipeValidator(
+        Level2LandingPageValidator.create(),
+        Level2MetadataResourceValidator.create(),
+        Level2ContentResourceValidator.create()
+    );
+  }
+
+  static Level2RecipeValidator create(
+      SignPostingValidator landingPageValidator,
+      SignPostingValidator metadataResourceValidator,
+      SignPostingValidator contentResourceValidator
+      ) {
+    return new Level2RecipeValidator(
+        landingPageValidator,
+        metadataResourceValidator,
+        contentResourceValidator);
+  }
+
 
   @Override
   public SignPostingResult validate(List<WebLink> webLinks) {
